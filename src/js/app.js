a24006a9a79c7c123c4d5cd62bd799430b1b0db9
Max Kyrchanov <max.kyrchanov@gmail.com>
2024-02-14 22:06:31 +0500

    init commit

--- src/js/app.js
+++
@@ -1,6 +0,0 @@
-import popoverToggle from "./popover";
-import registerEvent from "./eventsFuncs";
-
-registerEvent(".toggle-button__button", "click", () => {
-	popoverToggle(event, ".toggle-button");
-});
